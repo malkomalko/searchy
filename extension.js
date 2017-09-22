6661336d97ea9939ae1836d447a38d56ddfa819b
Robert Malko <rmalko@doximity.com>
2017-09-22 14:19:06 -0700

    start to capture user input

--- extension.js
+++ extension.js
@@ -1,8 +1,16 @@
 var vscode = require('vscode')
+var rgPath = require('vscode-ripgrep').rgPath
 
 function activate(context) {
   var disposable = vscode.commands.registerCommand('searchy.search', function () {
-    vscode.window.showInformationMessage('Searchy - Search')
+    vscode.window.showInputBox({
+      value: null,
+      prompt: "Uses ripgrep. e.g [foo -g 'README.*']",
+      placeHolder: "Search term...",
+      password: false
+    }).then((output) => {
+      console.log(`rgPath: ${rgPath}, output: ${output}`)
+    })
   })
 
   context.subscriptions.push(disposable)
