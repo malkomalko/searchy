b62c721218fe7c1005586fd0a47a9c5422408349
Robert Malko <rmalko@doximity.com>
2017-09-22 18:18:19 -0700

    reveal in range

--- commands.js
+++ commands.js
@@ -3,8 +3,16 @@ const rootPath = vscode.workspace.rootPath
 
 module.exports = {
   openFile: (params) => {
-    const filePath = `${rootPath}/${params.fileName}`
+    let filePath = `${rootPath}/${params.fileName}`
     var document = vscode.workspace.openTextDocument(filePath)
-    vscode.window.showTextDocument(document, 1)
+    vscode.window.showTextDocument(document, 1).then(() => {
+      if (params.line) {
+        let revealType = vscode.TextEditorRevealType.InCenter
+        let editor = vscode.window.activeTextEditor
+        let range = editor.document.lineAt(params.line - 1).range
+        editor.selection = new vscode.Selection(range.start, range.end)
+        editor.revealRange(range, revealType)
+      }
+    })
   }
 }
